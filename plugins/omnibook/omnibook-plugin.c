352354612aa4d6db915dc2c667be44e3e82c8182
info-cppsp <info@cppsp.de>
2017-11-18 16:20:36 +0100

    Fix indents 03

--- plugins/omnibook/omnibook-plugin.c
+++ plugins/omnibook/omnibook-plugin.c
@@ -33,75 +33,74 @@ const gchar *plugin_name = "omnibook";
 #define OMNIBOOK_DEVICE_FILE "/proc/omnibook/temperature"
 
 enum {
-	OMNIBOOK_DEVICE_FILE_OPEN_ERROR,
-	OMNIBOOK_DEVICE_FILE_READ_ERROR
+    OMNIBOOK_DEVICE_FILE_OPEN_ERROR,
+    OMNIBOOK_DEVICE_FILE_READ_ERROR
 };
 
 static void omnibook_plugin_setup_manually(GList **sensors) {
-	/* omnibook only has the one device file with one temp in it */
-	if (g_file_test(OMNIBOOK_DEVICE_FILE, G_FILE_TEST_EXISTS)) {
+    /* omnibook only has the one device file with one temp in it */
+    if (g_file_test(OMNIBOOK_DEVICE_FILE, G_FILE_TEST_EXISTS)) {
 
-		sensors_applet_plugin_add_sensor(sensors,
-                                                 OMNIBOOK_DEVICE_FILE,
-                                                 _("temperature"),
-                                                 _("CPU"),
-                                                 TEMP_SENSOR,
-                                                 TRUE,
-                                                 CPU_ICON,
-                                                 DEFAULT_GRAPH_COLOR);
-	}
-}	
+        sensors_applet_plugin_add_sensor(sensors,
+                                         OMNIBOOK_DEVICE_FILE,
+                                         _("temperature"),
+                                         _("CPU"),
+                                         TEMP_SENSOR,
+                                         TRUE,
+                                         CPU_ICON,
+                                         DEFAULT_GRAPH_COLOR);
+    }
+}
 
 
 /* to be called to setup for sys sensors */
 static GList *omnibook_plugin_init(void) {
-        GList *sensors = NULL;
-        /* call function to recursively look for sensors
-	   starting at the defined base directory */
-	omnibook_plugin_setup_manually(&sensors);
-        return sensors;
-		
+    GList *sensors = NULL;
+    /* call function to recursively look for sensors starting at the defined base directory */
+    omnibook_plugin_setup_manually(&sensors);
+
+    return sensors;
 }
 
-
-static gdouble omnibook_plugin_get_sensor_value(const gchar *path, 
-						  const gchar *id, 
-						  SensorType type,
-						  GError **error) {
-
-	/* to open and access the value of each sensor */
-	FILE *fp;
-	gfloat sensor_value;
-
-	if (NULL == (fp = fopen(path, "r"))) {
-		g_set_error(error, SENSORS_APPLET_PLUGIN_ERROR, OMNIBOOK_DEVICE_FILE_OPEN_ERROR, "Error opening sensor device file %s", path);
-		return -1.0;
-	}
-
-	if (fscanf(fp, "CPU temperature: %f", &sensor_value) != 1) {
-		g_set_error(error, SENSORS_APPLET_PLUGIN_ERROR, OMNIBOOK_DEVICE_FILE_READ_ERROR, "Error reading from sensor device file %s", path);
-		fclose(fp);
-		return -1.0;
-	}
-
-	fclose(fp);
-
-	return (gdouble)sensor_value;
-}
-
-const gchar *sensors_applet_plugin_name(void) 
-{
-        return plugin_name;
-}
-
-GList *sensors_applet_plugin_init(void) 
-{
-        return omnibook_plugin_init();
-}
-
-gdouble sensors_applet_plugin_get_sensor_value(const gchar *path, 
-                                                const gchar *id, 
+static gdouble omnibook_plugin_get_sensor_value(const gchar *path,
+                                                const gchar *id,
                                                 SensorType type,
                                                 GError **error) {
-        return omnibook_plugin_get_sensor_value(path, id, type, error);
+
+    /* to open and access the value of each sensor */
+    FILE *fp;
+    gfloat sensor_value;
+
+    if (NULL == (fp = fopen(path, "r"))) {
+        g_set_error(error, SENSORS_APPLET_PLUGIN_ERROR, OMNIBOOK_DEVICE_FILE_OPEN_ERROR, "Error opening sensor device file %s", path);
+        return -1.0;
+    }
+
+    if (fscanf(fp, "CPU temperature: %f", &sensor_value) != 1) {
+        g_set_error(error, SENSORS_APPLET_PLUGIN_ERROR, OMNIBOOK_DEVICE_FILE_READ_ERROR, "Error reading from sensor device file %s", path);
+        fclose(fp);
+        return -1.0;
+    }
+
+    fclose(fp);
+
+    return (gdouble)sensor_value;
+}
+
+const gchar *sensors_applet_plugin_name(void)
+{
+    return plugin_name;
+}
+
+GList *sensors_applet_plugin_init(void)
+{
+    return omnibook_plugin_init();
+}
+
+gdouble sensors_applet_plugin_get_sensor_value(const gchar *path,
+                                                const gchar *id,
+                                                SensorType type,
+                                                GError **error) {
+
+    return omnibook_plugin_get_sensor_value(path, id, type, error);
 }
