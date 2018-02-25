7e62b22bcdbcc6428a5a5d01552e1308a6854cee
info-cppsp <info@cppsp.de>
2018-02-25 20:07:55 +0100

    UDisks2 plugin cleanup
    comments cleanup
    init ptrs to NULL
    added whitespace

--- plugins/udisks2/udisks2-plugin.h
+++ plugins/udisks2/udisks2-plugin.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 Pramod Dematagoda <pmd.lotr.gandalf@gmail.com>
+ * Copyright (C) 2017 info-cppsp <info@cppsp.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
