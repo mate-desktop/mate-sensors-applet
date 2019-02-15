2628689226d0f3c087d872fe54fb63bf83db3c56
rbuj <robert.buj@gmail.com>
2019-02-15 14:05:00 +0100

    Make translatable the copyright in about dialog

--- sensors-applet/about-dialog.c
+++ sensors-applet/about-dialog.c
@@ -37,7 +37,9 @@ void about_dialog_open(SensorsApplet *sensors_applet) {
                   "icon-name", "mate-sensors-applet",
                   "program-name", PACKAGE_NAME,
                   "version", PACKAGE_VERSION,
-                  "copyright", "(C) 2005-2009, Alex Murray <murray.alex@gmail.com>\n(C) 2011, Stefano Karapetsas <stefano@karapetsas.com>\n(C) 2012-2018, MATE developers",
+                  "copyright", _("Copyright \xc2\xa9 2005-2009 Alex Murray\n"
+                                 "Copyright \xc2\xa9 2011 Stefano Karapetsas\n"
+                                 "Copyright \xc2\xa9 2012-2019 MATE developers"),
                   "authors", authors,
                   "documenters", authors,
                   /* To translator: Put your name here to show up in the About dialog as the translator */
